92cc2225cba18e3ee2a7501db970b770bddd2c4a
Snehal Patil <sneha.swapnil1996@gmail.com>
2021-10-11 15:04:37 +0530

    regex UC 3 is added

---
+++ Regex_UC3.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexDay19
+{
+    class UserInfo
+    {
+        //UC1
+        public static string FirstName = "^[A-Z][a-z]{3,}$";
+
+        public bool validateFirstName(string Fname)
+        {
+            return Regex.IsMatch(Fname, FirstName);
+        }
+
+        //UC2
+        public static string LastName = "^[A-Z][a-z]{3,}$";
+
+        public bool validateLastName(string Lname)
+        {
+            return Regex.IsMatch(Lname, FirstName);
+        }
+
+        //UC3 
+        public static string Email = "^[a-zA-Z0-9]+([.#_$+-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3})?$";
+
+        public bool validateEmail(string mail)
+        {
+            return Regex.IsMatch(mail, Email);
+        }
+    }
+}
