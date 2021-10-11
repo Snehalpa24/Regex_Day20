0b9692ad5a9a4e0206f152cd5e991c510fa95f66
Snehal Patil <sneha.swapnil1996@gmail.com>
2021-10-11 15:00:20 +0530

    Regex uc1 is added

---
+++ Regx_UC1.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexDay19
+{
+    class UserInfo
+    {
+        //UC1
+        public static string FirstName = "^[A-Z][a-z]{3,}$";
+
+        public bool validateFirstName(string Fname)
+        {
+            return Regex.IsMatch(Fname, FirstName);
+        }
+    }
+}
